b8298ddf369d81220206b8eff74649ab09ea99a6
Cristian Cretu <crisemcr@gmail.com>
2023-03-24 22:12:16 +0200

    feat: fetch companies:

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Just a personal list of companies I'd like to work at. I'm not affiliated with a
 
 - [The Browser Company](https://arc.net/)
   - Built one of the juiciest piece of software I've ever used. "Building the personal internet computer"
+- [Linear](https://linear.app/)
+  - A modern and fast issue tracking tool for software teams
+- [Craft](https://www.craft.do/)
+  - A flexible, collaborative document editor for teams
+- [Loom](https://www.loom.com/)
+  - A video messaging tool for remote teams
+- [Cron](https://cron.com/)
+  - A platform for creating and managing scheduled tasks and workflows
+- [Superhuman](https://superhuman.com/)
+  - An email client designed for productivity and speed
+- [Raycast](https://www.raycast.com/)
+  - A productivity tool for developers, allowing you to control your tools with a keyboard and search.
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,56 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+const url =
+  "https://raw.githubusercontent.com/cristicretu/coolcompaniestoworkat/main/README.md";
+
+interface Company {
+  title: string;
+  link: string;
+  description: string;
+}
+
+function parseMarkdown(markdown: string): Company[] {
+  const lines = markdown.split("\n");
+  const companies: Company[] = [];
+  let company: Company | null = null;
+
+  for (const line of lines) {
+    if (line.startsWith("- [")) {
+      console.log(line);
+      const title = line.match(/\[(.*?)\]/)?.[1] || "";
+      const link = line.match(/\((.*?)\)/)?.[1] || "";
+      company = { title, link, description: "" };
+      companies.push(company);
+    } else if (company && !line.startsWith("##") && line.trim()) {
+      company.description = `${company.description} ${line.trim()}`;
+    }
+  }
+
+  return companies;
+}
+
 export default component$(() => {
+  const companies = useSignal<Company[]>();
+
+  useTask$(async () => {
+    const response = await fetch(url);
+    const text = await response.text();
+    const parsedCompanies = parseMarkdown(text);
+    companies.value = parsedCompanies;
+  });
+
   return (
     <>
-      <div class="section bright">
-        <div class="container center">ey mate</div>
+      <div>
+        {companies.value?.map((company, index) => (
+          <div key={index}>
+            <a href={company.link} rel="noopener noreferrer" target="_blank">
+              {company.title}
+            </a>
+            <p>{company.description}</p>
+          </div>
+        ))}
       </div>
     </>
   );
